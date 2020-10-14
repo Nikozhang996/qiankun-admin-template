f74e188d15da92593ea0b5d2b930de35fda2c861
zhangjianke <593177876@qq.com>
2020-10-14 20:45:01 +0800

    feat: 首次跑通

--- project-base/src/main.js
+++ project-base/src/main.js
@@ -1,14 +1,16 @@
 import Vue from "vue";
 import ElementUI from "element-ui";
 
+import App from './App.vue'
+
 Vue.use(ElementUI, {
   size: "mini",
 });
 
 Vue.config.productionTip = false;
 
-new Vue({
+export default new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount("#root");
 
-export default application;
+
