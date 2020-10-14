20d7f93ca5aa9a5f16ea5f22a72a7d55069e187b
zhangjianke <593177876@qq.com>
2020-10-14 18:36:52 +0800

    feat: add build files

---
+++ project-base/build/webpack.dll.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const DllPlugin = require("webpack/lib/DllPlugin");
+
+module.exports = {
+  mode: "production",
+  entry: ["react", "react-dom"],
+  output: {
+    library: "react",
+    libraryTarget: "commonjs",
+    filename: "dll.js", //打包后接收的名字
+    path: path.resolve(__dirname, "../public"), //打包后地址
+  },
+  plugins: [
+    new DllPlugin({
+      name: "react",
+      path: path.resolve(__dirname, "./dll/manifest.json"),
+    }),
+  ],
+};
